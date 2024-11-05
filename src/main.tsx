0feced5b5b4fad095e0fc841baf6b93e8e6101e8
renatorrocha <renatorrodrigues2002@gmail.com>
2024-11-04 22:56:15 -0300

    feat: setup deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/global.css";
-import App from '@components/providers/app';
+import App from "@components/providers/app";
 
 const container = document.getElementById("root");
 
 if (container) {
     const root = createRoot(container);
 
-root.render(
-  <StrictMode>
-    <App/>
-  </StrictMode>
-)
-
+    root.render(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), TanStackRouterVite()],
-    resolve: {
-        alias: {
-            "@assets": path.resolve(__dirname, "./src/assets"),
-            "@components": path.resolve(__dirname, "./src/components"),
-            "@lib": path.resolve(__dirname, "./src/lib"),
-            "@services": path.resolve(__dirname, "./src/services"),
-            "@constants": path.resolve(__dirname, "./src/constants"),
-            "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    const BASE_URL = env.VITE_BASE_URL || "/";
+    const APP_PORT = Number(env.VITE_APP_PORT) || 5173;
+
+    return {
+        base: BASE_URL,
+        server: {
+            port: APP_PORT,
         },
-    },
+        plugins: [react(), TanStackRouterVite()],
+        resolve: {
+            alias: {
+                "@assets": path.resolve(__dirname, "./src/assets"),
+                "@components": path.resolve(__dirname, "./src/components"),
+                "@lib": path.resolve(__dirname, "./src/lib"),
+                "@services": path.resolve(__dirname, "./src/services"),
+                "@constants": path.resolve(__dirname, "./src/constants"),
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+    };
 });
